9aac7a8a308d9aa20a2c5cf9eb4f75ce5ee6ff3e
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 03:17:51 +0330

    add(accounts.admin): register user model to admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounts.models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'pk')
+    search_fields = ('username',)
