ea3051c8f7fa83201f305caf141ee5ed86d24c4e
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 14:45:05 +0330

    add: add pin APIs to urls.

--- pinspire_project/urls.py
+++ pinspire_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.urls import urlspatterns as AcountAPI
+from core.urls import urlpatterns as CoreAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(AcountAPI)),
+    path('api/', include(CoreAPI)),
 ]
