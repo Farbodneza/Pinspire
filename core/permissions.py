86111161831b7424ffbe6d071d6c3fcb99fdde35
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 14:00:07 +0330

    add(core.permissions): add IsPinOwner permission.

--- core/permissions.py
+++ core/permissions.py
@@ -1,1 +1,7 @@
 from rest_framework.permissions import BasePermission
+
+
+class IsPonOwnerPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return user.user_pin == obj
