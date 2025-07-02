2c5e09abf83b3f1e060935f6fe383148d7ab8a84
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 14:39:04 +0330

    add(settings): handelling permissions.

--- pinspire_project/settings.py
+++ pinspire_project/settings.py
@@ -116,3 +116,10 @@ import os
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
