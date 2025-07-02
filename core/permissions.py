6274faafdd2ddef93f1cd7a2ee0486dbb793e86b
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 14:40:18 +0330

    fix(core.permissions): fix IsPinOwner perm.

--- core/permissions.py
+++ core/permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsPonOwnerPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
-        return user.user_pin == obj
+        return user == obj.owner
