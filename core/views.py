6339f4b7927d8581d583c6b50f91b6a02ea240d5
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 18:23:02 +0330

    add(core.views): add BoardAPIViewset.

--- core/views.py
+++ core/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
-from core.models import Pin
-from core.serializers import PinSerializer
+from core.models import Pin, Board
+from core.serializers import PinSerializer, BoardSerializer
 from rest_framework.response import Response
 from rest_framework import viewsets, filters
+from core.permissions import IsPonOwnerPermission, IsBoardOwner
+from rest_framework.permissions import IsAuthenticated , AllowAny
 
 
-class PinAPIViewset(viewsets.ModelViewSet, generics.GetOrRaiseMixin):
+class PinAPIViewset(viewsets.ModelViewSet):
     queryset = Pin.objects.all()
     serializer_class = PinSerializer
     search_fields = ['title', 'tags']
     lookup_field = "pk"
     lookup_url_kwarg = 'pk'
+
+    
     def get_permissions(self):
         if self.action == 'list':
-            permission_classes = ["AllowAny"]
+            permission_classes = [AllowAny]
         elif self.action in ['update', 'partial_update', 'destroy']:
-            permission_classe = ['Owner-only']
+            permission_classes = [IsPonOwnerPermission, IsAuthenticated]
         else:
-            permission_classes = ["IsAuthenticated"]
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
     
-    
+
     def searh(self, request, *args, **kwargs):
         queryset = self.get_queryset
         filter = filters.SearchFilter()
@@ -29,3 +34,19 @@ class PinAPIViewset(viewsets.ModelViewSet, generics.GetOrRaiseMixin):
         serializer = PinSerializer(result, many=True)
         return Response(serializer.data)
 
+
+class BoardAPIViewset(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    lookup_field = "pk"
+    lookup_url_kwarg = 'pk'
+
+
+    def get_permissions(self):
+        if self.action in ['list', 'crate']:
+            permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated, IsBoardOwner]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
