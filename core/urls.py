d235d6907edf48992964299cfedb10a0c5375d69
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 18:41:13 +0330

    add(core.urls): add board APIs to urls.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import PinAPIViewset  
+from core.views import PinAPIViewset, BoardAPIViewset
 
 router = DefaultRouter()
 router.register(r'pins', PinAPIViewset, basename='pin')
+router.register(r'boards', BoardAPIViewset, basename='board')
 
 urlpatterns = [
     path('', include(router.urls)),
