85d617a1f00c920c37c0d4fcc250223bf1241caa
Farbod Nezafati <farbodneza@gmail.com>
2025-07-02 14:41:07 +0330

    add(core.urls): add pin APIs to urlpatterns.

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import PinAPIViewset  
+
+router = DefaultRouter()
+router.register(r'pins', PinAPIViewset, basename='pin')
 
 urlpatterns = [
-    path('auth/register/', views.register_user, name='register_user'),
-    path('auth/login/', views.login_user, name='login_user'),
-
-    path('pins/', views.list_pins, name='list_pins'),
-    path('pins/upload/', views.upload_pin, name='upload_pin'),
-    path('pins/<int:pin_id>/', views.pin_detail, name='pin_detail'),
+    path('', include(router.urls)),
 ]
