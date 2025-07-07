f27a0da508d0602aa3314e3046b85fba8d0d1a4d
Farbod Nezafati <farbodneza@gmail.com>
2025-07-07 13:10:43 +0330

    end of project

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from accounts.models import CustomUser
 
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+
 class CustomuserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -14,8 +19,12 @@ class CustomuserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-    # to repersetiotion
-    # to internall valeu
+    
+    def validate(self, attrs):
+        if attrs["username"] == attrs["password"]:
+            raise serializers.ValidationError('username and password should be deferent')
+        return attrs
+    
     
 class CustomuserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
